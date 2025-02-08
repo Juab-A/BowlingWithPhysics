2c3f0ec832e3dfe363e8f9b38f981af025f1e547
Juan Ablan <ablanjuan1@gmail.com>
2025-02-07 18:43:19 -0800

    Added Ball, Surface and Space Motion
    Spacebar to move the ball forward

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody>(); //Grabs reference to Rigidboy
+
+        //Adds a listener to the OnSpacePressed Event
+        //When space pressed, LaunchBall method called
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+
+    }
+
+    private void LaunchBall() {
+        ballRb.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
