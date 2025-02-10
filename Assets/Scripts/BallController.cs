55a6f2933134271d0c9bb16c2bce79fd487452bf
Juan Ablan <ablanjuan1@gmail.com>
2025-02-09 18:23:41 -0800

    Added Reset Functionality

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -24,6 +24,9 @@ public class BallController : MonoBehaviour
         transform.localPosition = Vector3.zero;
         ballRb.isKinematic = true;
 
+        Cursor.lockState = CursorLockMode.Locked;
+        ResetBall();
+
     }
 
     private void LaunchBall() {
@@ -33,6 +36,15 @@ public class BallController : MonoBehaviour
         ballRb.isKinematic = false; //Set "IsKinematic" to false so that the ball can collide/perform via physics engine
         ballRb.AddForce(launchIndicator.transform.forward * force, ForceMode.Impulse);
         launchIndicator.gameObject.SetActive(false);
+    }
+
+    public void ResetBall() {
+        isBallLaunched = false;
+
+        ballRb.isKinematic = false;
+        launchIndicator.gameObject.SetActive(true);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
     }
 
     // Update is called once per frame
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,18 +5,49 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
+
+
+
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Find all objects of type FallTrigger
-        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        inputManager.OnResetPressed.AddListener(HandleReset);
 
-        foreach (FallTrigger pin in pins) {
-            pin.OnPinFall.AddListener(IncrementScore);
+    }
+
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins() {
+
+        if (pinObjects) { //Assure there are no pins already there, we destroy them if there are
+            foreach (Transform child in pinObjects.transform) {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
         }
 
+        //Instantiate a new set of pins
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+
+        //Add the Increment Score function as a listener
+        //to the OnPinfall Event of each new pin
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (FallTrigger pin in fallTriggers) {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
     }
 
     private void IncrementScore() {
